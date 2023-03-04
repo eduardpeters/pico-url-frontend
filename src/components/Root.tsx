553ec78947257de78101c23fbafaf857ed95f893
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-03-04 05:28:21 +0100

    Adding initial Root styles

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,15 +1,23 @@
 import { Link } from "react-router-dom";
 import RedirectForm from "./RedirectForm";
+import styles from "../styles/general.module.css";
 import "../styles/Root.css";
 
 function Root() {
     return (
-        <div className="Root">
-            <div className="root__content">
-                <h1>Welcome to Pico URL!</h1>
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <h1 className={styles.title}>Welcome to Pico URL!</h1>
+                <p className={styles.text}>A minimalistic URL shortening service</p>
+                <h3 className={styles.subtitle}>Begin your journey right away</h3>
+                <p>Use the form below for your Pico URL link or enter it directly in your browser's navigation bar</p>
                 <RedirectForm />
-                <div>Create an account or log in</div>
-                <Link to="/login">Log In!</Link>
+                <h3 className={styles.subtitle}>Create or log into your account</h3>
+                <p>Only registered members can create and manage Pico URLs</p>
+                <div className={styles.links}>
+                    <Link to="/login">Log In!</Link>
+                    <Link to="/register">Register!</Link>
+                </div>
             </div>
         </div>
     );
