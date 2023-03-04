fb05b4ff695f3452ba77ef8a0a2f89decf758f86
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-03-04 04:30:42 +0100

    Restructured assets to styles

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 import RedirectForm from "./RedirectForm";
+import "../styles/Root.css";
 
 function Root() {
     return (
         <div className="Root">
-            <h1>Welcome to Pico URL!</h1>
-            <RedirectForm />
-            <div>Create an account or log in</div>
-        <Link to="/login">Log In!</Link>
+            <div className="root__content">
+                <h1>Welcome to Pico URL!</h1>
+                <RedirectForm />
+                <div>Create an account or log in</div>
+                <Link to="/login">Log In!</Link>
+            </div>
         </div>
     );
 }
