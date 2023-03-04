2b5607c770e01f6b66c60598bb34cbb160afe2b5
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-03-04 13:23:15 +0100

    More Root styling

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -7,16 +7,22 @@ function Root() {
     return (
         <div className={styles.container}>
             <div className={styles.content}>
-                <h1 className={styles.title}>Welcome to Pico URL!</h1>
-                <p className={styles.text}>A minimalistic URL shortening service</p>
-                <h3 className={styles.subtitle}>Begin your journey right away</h3>
-                <p>Use the form below for your Pico URL link or enter it directly in your browser's navigation bar</p>
-                <RedirectForm />
-                <h3 className={styles.subtitle}>Create or log into your account</h3>
-                <p>Only registered members can create and manage Pico URLs</p>
-                <div className={styles.links}>
-                    <Link to="/login">Log In!</Link>
-                    <Link to="/register">Register!</Link>
+                <div className="root__container">
+                    <h1 className={styles.title}>Welcome to Pico URL!</h1>
+                    <p className={styles.text}>A minimalistic URL shortening service</p>
+                </div>
+                <div className="root__container">
+                    <h3 className={`${styles.subtitle} unhide`}>Begin your journey right away</h3>
+                    <p className="hidden root__paragraph">Use the form below for your Pico URL or enter it directly in your browser's navigation bar</p>
+                    <RedirectForm />
+                </div>
+                <div className="root__container">
+                    <h3 className={styles.subtitle}>Create or log into your account</h3>
+                    <p className="root__paragraph">Only registered members can create and manage Pico URLs</p>
+                    <div className={styles.links}>
+                        <Link to="/login">Log In!</Link>
+                        <Link to="/register">Register!</Link>
+                    </div>
                 </div>
             </div>
         </div>
