fc4c74642b5c5b640f12f56069a2fb1af73a5231
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-03-08 19:23:19 +0100

    Basic Login component styling

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { authAPI } from "../services/authAPI";
+import styles from "../styles/general.module.css";
+import "../styles/Login.css";
 
 function Login() {
-    const [email, setEmail] = useState("");    
-    const [password, setPassword] = useState("");    
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
 
 
@@ -20,16 +23,26 @@ function Login() {
     }
 
     return (
-        <div className="Login">
-            {errorMessage.length ? <h3>{errorMessage}</h3> : null}
-            <h3>Please enter your e-mail and password</h3>
-            <form onSubmit={event => handleLogIn(event)}>
-                <label htmlFor="email">E-Mail:</label>
-                <input type="email" id="email" required onChange={event => setEmail(event.target.value)}></input>
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" required onChange={event => setPassword(event.target.value)}></input>
-                <button type="submit">Log In</button>
-            </form>
+        <div className={styles.container}>
+            <div className="login__content">
+                <h3 className={styles.subtitle}>Please enter your e-mail and password</h3>
+                <p className="form__error">{errorMessage}</p>
+                <form className="form__container" onSubmit={event => handleLogIn(event)}>
+                    <div className="form__field">
+                        <label htmlFor="email">E-Mail</label>
+                        <input type="email" id="email" required onChange={event => setEmail(event.target.value)}></input>
+                    </div>
+                    <div className="form__field">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" required onChange={event => setPassword(event.target.value)}></input>
+                    </div>
+                    <button className="form__button" type="submit">Log In!</button>
+                </form>
+                <div className={styles.links}>
+                    <Link to="/">Return home</Link>
+                    <Link to="/register">Register!</Link>
+                </div>
+            </div>
         </div>
     );
 }
