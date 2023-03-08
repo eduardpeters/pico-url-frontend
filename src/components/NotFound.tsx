f9d05f0e3ae9c6e88036c2cdb8cb8da776e7d7ca
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-03-08 18:37:01 +0100

    Fixing NotFound in case there is no error state

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -7,7 +7,7 @@ function NotFound() {
 
     return (
         <div className={styles.container}>
-            <h1 className="not-found__error">{state.error}</h1>
+            <h1 className="not-found__error">{state?.error || "Not Found"}</h1>
             <h3>Thank you for using Pico URL</h3>
             <h3>Sadly, we could not retrieve the page you were looking for</h3>
             <div className={styles.links}>
