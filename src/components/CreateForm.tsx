a6537b68d430d2d2d67b9350edd7993107a51c81
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-03-11 13:53:37 +0100

    Handling the two positive create URL cases

--- src/components/CreateForm.tsx
+++ src/components/CreateForm.tsx
@@ -1,5 +1,13 @@
 import { useState } from "react";
 import { useAuthContext } from "../context/AuthContext";
+import { urlsAPI } from "../services/urlsAPI";
+
+interface CreateResponseInterface {
+    status: number;
+    data: {
+        shortUrl: string;
+    }
+}
 
 function CreateForm() {
     const authContext = useAuthContext();
@@ -7,16 +15,38 @@ function CreateForm() {
 
     async function handleCreateSubmit(event: React.FormEvent) {
         event.preventDefault();
-        console.log("Submitted!!");
-        if (originalUrl) {
-            console.log("Sent to backend");
+        if (originalUrl && authContext?.userDetails?.token) {
+            const response = await urlsAPI.postUrl(authContext?.userDetails?.token, originalUrl);
+            if (!(response as {error: string}).error) {
+                showCreateResult((response as CreateResponseInterface).status, (response as CreateResponseInterface).data.shortUrl);
+                setOriginalUrl("");
+            }
+        }
+    }
+
+    function showCreateResult(status: number, shortUrl: string) {
+        switch (status) {
+            case (200):
+                console.log("Pico URL already exists, this is it: ", shortUrl);
+                break;
+            case (201):
+                console.log("New PICO URL created: ", shortUrl);
+                break;
+            default:
+                console.error("Unexpected status");
         }
     }
 
     return (
         <form onSubmit={event => handleCreateSubmit(event)}>
             <label>URL to shorten</label>
-            <input type="url" onChange={event => setOriginalUrl(event.target.value)}></input>
+            <input
+                type="url"
+                value={originalUrl}
+                onChange={event => setOriginalUrl(event.target.value)}
+                required
+            >
+            </input>
             <button type="submit">Minify!</button>
         </form>
     );
--- src/services/urlsAPI.ts
+++ src/services/urlsAPI.ts
@@ -31,4 +31,22 @@ async function getCount(userToken: string) {
     }
 }
 
-export const urlsAPI = { getOriginal, getCount };
+async function postUrl(userToken: string, originalUrl: string) {
+    const requestUrl = `${baseUrl}urls`;
+    const requestBody = { url: originalUrl }
+    const config = {
+        headers: {
+            Authorization: `Bearer ${userToken}`
+        }
+    }
+    try {
+        const response = await axios.post(requestUrl, requestBody, config);
+        return response;
+    }
+    catch (error) {
+        console.error(error);
+        return {error: (error as AxiosError).response?.data || (error as AxiosError).message };
+    }
+}
+
+export const urlsAPI = { getOriginal, getCount, postUrl };
