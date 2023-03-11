2112976780a911cf8b9a4c908426e47fb8828d3e
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-03-11 11:44:48 +0100

    Retrieving URL count in dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../context/AuthContext";
+import { urlsAPI } from "../services/urlsAPI";
 import "../styles/Dashboard.css";
 
 function Dashboard() {
+    const authContext = useAuthContext();
+    const navigate = useNavigate();
+    const [urlCount, setUrlCount] = useState(0);
+
+    useEffect(() => {
+        async function getUrlCount(userToken: string) {
+            const response = await urlsAPI.getCount(userToken);
+            if (!response.error) {
+                setUrlCount(response.count);
+            }
+        }
+        if (!authContext?.isLoggedIn || !authContext.userDetails?.token) {
+            console.log("User not logged in, redirecting");
+            navigate("/");
+        } else {
+            getUrlCount(authContext.userDetails?.token);
+        }
+    }, []);
+
     return (
         <div className="dashboard__container">
             <h1 className="dashboard__title">Pico URL Dashboard</h1>
+            <div>{`You have ${urlCount} URLs`}</div>
             <div className="dashboard__upper">
                 <div>User Statistics</div>
                 <div>Create a picoUrl</div>
--- src/services/urlsAPI.ts
+++ src/services/urlsAPI.ts
@@ -14,4 +14,21 @@ async function getOriginal(shortId: string) {
     }
 }
 
-export const urlsAPI = { getOriginal };
+async function getCount(userToken: string) {
+    const requestUrl = `${baseUrl}urls/count`;
+    const config = {
+        headers: {
+            Authorization: `Bearer ${userToken}`
+        }
+    }
+    try {
+        const response = await axios.get(requestUrl, config);
+        return response.data;
+    }
+    catch (error) {
+        console.error(error);
+        return {error: (error as AxiosError).response?.data || (error as AxiosError).message };
+    }
+}
+
+export const urlsAPI = { getOriginal, getCount };
