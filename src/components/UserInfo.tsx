0c6c7385173dac69ff94863c034338ed247d33f5
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-03-13 19:47:16 +0100

    Adding logout logic to UserInfo

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "../context/AuthContext";
 import "../styles/UserInfo.css";
 
@@ -7,12 +8,19 @@ interface UserInfoProps {
 
 function UserInfo({ urlCount }: UserInfoProps) {
     const authContext = useAuthContext();
+    const navigate = useNavigate();
+
+    function handleLogout() {
+        authContext?.setUserDetails(null);
+        authContext?.setIsLoggedIn(false);
+        navigate("/");
+    }
 
     return (
         <div className="info__container">
             <h3>Hello, <span className="info__highlight">{authContext?.userDetails?.name}</span>!</h3>
-            <p>You have <span className="info__highlight">{urlCount}</span> Pico URLs</p>
-            <button className="info__logout">Log Out!</button>
+            <p>You have <span className="info__highlight">{urlCount}</span>{` Pico URL${urlCount !== 1 ? 's' : ''}`}</p>
+            <button className="info__logout" onClick={handleLogout}>Log Out!</button>
             <p className="info__edit">Change E-Mail or Password</p>
         </div>
     );
