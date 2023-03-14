b20b86467d906b5ef3a273f1d6f311b451deb952
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-03-14 17:49:06 +0100

    Adding color scheme to icons in UrlEntry

--- src/components/UrlEntry.tsx
+++ src/components/UrlEntry.tsx
@@ -1,6 +1,6 @@
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import LaunchIcon from "@mui/icons-material/Launch";
 import { UrlInterface } from "../types/picotypes";
 import "../styles/UrlEntry.css";
@@ -17,25 +17,25 @@ function UrlEntry({ entry }: UrlEntryProps) {
         <div className="entry__container">
             <div className="entry__summary">
                 <div className="summary__pico">
-                    <h3>Pico: {entry.shortUrl}</h3>
-                    <ContentCopyIcon className="entry__icon" onClick={() => navigator.clipboard.writeText(entry.shortUrl)} />
+                    <h3>Pico: <span className="entry__highlight">{entry.shortUrl.slice(-10)}</span></h3>
+                    <ContentCopyIcon className="entry__icon entry__icon-copy" onClick={() => navigator.clipboard.writeText(entry.shortUrl)} />
                 </div>
                 <div className="summary__long">
                     <h4>Long: {entry.originalUrl.substring(0, 30)}...</h4>
-                    <LaunchIcon className="entry__icon" />
+                    <LaunchIcon className="entry__icon entry__icon-open" />
                 </div>
             </div>
             {
                 showDetails ?
                     <>
-                        <KeyboardArrowDownIcon className="entry__icon" onClick={() => setShowDetails(false)} />
+                        <ExpandLessIcon fontSize="large" className="entry__icon entry__icon-details" onClick={() => setShowDetails(false)} />
                         <div className="entry__details">
                             <p>Visits: {entry.visits}</p>
                             <p>Created on: {entry.date}</p>
                         </div>
                     </>
                     :
-                    <KeyboardArrowRightIcon className="entry__icon" onClick={() => setShowDetails(true)} />
+                    <ExpandMoreIcon fontSize="large" className="entry__icon entry__icon-details" onClick={() => setShowDetails(true)} />
             }
         </div>
     );
