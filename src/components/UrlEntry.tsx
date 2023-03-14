55a913782c0e25a65603a00a41e0ec1f6c01f7eb
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-03-14 18:19:55 +0100

    Adding style to UrlEntry Details

--- src/components/UrlEntry.tsx
+++ src/components/UrlEntry.tsx
@@ -12,17 +12,20 @@ interface UrlEntryProps {
 
 function UrlEntry({ entry }: UrlEntryProps) {
     const [showDetails, setShowDetails] = useState(false);
+    const entryDate = new Date(entry.date);
 
     return (
         <div className="entry__container">
             <div className="entry__summary">
                 <div className="summary__pico">
-                    <h3>Pico: <span className="entry__highlight">{entry.shortUrl.slice(-10)}</span></h3>
+                    <h3>Pico: <span className="summary__highlight">{entry.shortUrl.slice(-10)}</span></h3>
                     <ContentCopyIcon className="entry__icon entry__icon-copy" onClick={() => navigator.clipboard.writeText(entry.shortUrl)} />
                 </div>
                 <div className="summary__long">
-                    <h4>Long: {entry.originalUrl.substring(0, 30)}...</h4>
-                    <LaunchIcon className="entry__icon entry__icon-open" />
+                    <h4>Long: <span className="summary__url">{entry.originalUrl.substring(0, 30)}...</span></h4>
+                    <a href={entry.originalUrl} target="_blank" rel="noreferrer noopener">
+                        <LaunchIcon className="entry__icon entry__icon-open" />
+                    </a>
                 </div>
             </div>
             {
@@ -30,8 +33,9 @@ function UrlEntry({ entry }: UrlEntryProps) {
                     <>
                         <ExpandLessIcon fontSize="large" className="entry__icon entry__icon-details" onClick={() => setShowDetails(false)} />
                         <div className="entry__details">
-                            <p>Visits: {entry.visits}</p>
-                            <p>Created on: {entry.date}</p>
+                            <p>Visits: <span className="details__highlight">{entry.visits}</span></p>
+                            <p>Redirects to: <span className="details__long">{entry.originalUrl}</span></p>
+                            <p>Created on: {entryDate.toDateString()}</p>
                         </div>
                     </>
                     :
