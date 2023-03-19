53850d5e66a237476ecf7d5a0381e55282152ab2
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-03-19 20:07:01 +0100

    Adding Delete confirmation buttons

--- src/components/UrlEntry.tsx
+++ src/components/UrlEntry.tsx
@@ -16,6 +16,10 @@ function UrlEntry({ entry }: UrlEntryProps) {
     const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
     const entryDate = new Date(entry.date);
 
+    function handleUrlDelete() {
+        console.log("URL will be deleted");
+    }
+
     return (
         <div className="entry__container">
             <div className="entry__summary">
@@ -38,8 +42,16 @@ function UrlEntry({ entry }: UrlEntryProps) {
                             <p>Visits: <span className="details__highlight">{entry.visits}</span></p>
                             <p>Redirects to: <span className="details__long">{entry.originalUrl}</span></p>
                             <p>Created on: {entryDate.toDateString()}</p>
-                            <DeleteForeverIcon fontSize="large" className="entry__icon entry__icon-delete" onClick={() => setShowDeleteConfirm(true)} />
-                            {showDeleteConfirm && <div>Show the modal</div>}
+                            <DeleteForeverIcon fontSize="large" className="entry__icon entry__icon-delete" onClick={() => setShowDeleteConfirm(!showDeleteConfirm)} />
+                            {
+                                showDeleteConfirm 
+                                && 
+                                <div className="delete__confirmation">
+                                    <p className="delete__text">Delete this Pico URL?</p>
+                                    <button className="delete__button delete__button-confirm" onClick={() => handleUrlDelete()}>Yep</button>
+                                    <button className="delete__button delete__button-cancel" onClick={() => setShowDeleteConfirm(!showDeleteConfirm)}>Nope</button>
+                                </div>
+                            }
                         </div>
                     </>
                     :
