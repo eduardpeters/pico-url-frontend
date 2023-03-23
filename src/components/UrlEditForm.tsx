26dafe24dfc07b8159896e6df75e428d10ecf15b
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-03-23 19:55:18 +0100

    Basic styling URL edit

--- src/components/UrlEditForm.tsx
+++ src/components/UrlEditForm.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { urlsAPI } from "../services/urlsAPI";
+import "../styles/UrlEditForm.css";
 
 interface UrlEditFormProps {
     shortUrl: string;
@@ -22,11 +23,11 @@ function UrlEditForm({ shortUrl, originalUrl, userToken, closeForm }: UrlEditFor
     }
 
     return (
-        <form onSubmit={event => handleUrlEdit(event)}>
-            <label htmlFor="url-input">New URL:</label>
-            <input id="url-input" type="url" value={newUrl} placeholder="new looong url" onChange={event => setNewUrl(event.target.value)}></input>
-            <button type="submit">Replace!</button>
-            <button type="button" onClick={() => closeForm()}>Nevermind</button>
+        <form className="url__form" onSubmit={event => handleUrlEdit(event)}>
+            <label className="url__label" htmlFor="url-input">New URL:</label>
+            <input className="url__input" id="url-input" type="url" value={newUrl} placeholder="new looong url" onChange={event => setNewUrl(event.target.value)}></input>
+            <button className="url__button url__button-replace" type="submit">Replace!</button>
+            <button className="url__button url__button-cancel" type="button" onClick={() => closeForm()}>Nevermind</button>
         </form>
     );
 }
